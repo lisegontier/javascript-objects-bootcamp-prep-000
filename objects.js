6847c541455a9ae9c525b2dab8f03b95caffc652
Liz Gunter <midwestunicorn@gmail.com>
2018-04-30 21:17:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,9 +7,9 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
-};
+}
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.Slowdive;
   return playlist;
-};
+}
